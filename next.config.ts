5191ffe670436bc9833afce8476c3cf8d4f7c00b
Sheenlow <bernacouderc@gmail.com>
2026-07-13 03:08:22 -0300

    Fix error 310 console attemp 3

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import { resolve } from "path";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const csp = [
   "default-src 'self'",
@@ -38,12 +37,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  widenClientFileUpload: true,
-  sourcemaps: {
-    deleteSourcemapsAfterUpload: true,
-  },
-});
+export default nextConfig;
